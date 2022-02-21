45024e7c6d962feb22025e8e7823369acbdee887
MistakenReptile <hari.k.reddy0905@gmail.com>
2022-02-21 22:26:36 +0530

    first commit

---
+++ src/com/basics/ArrayDemo.java
@@ -0,0 +1,22 @@
+package com.basics;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] anArray;
+        anArray =new int[10];
+
+        //First Element - Index=0
+        anArray[0]=100;
+        // and so on
+        anArray[1]=200;
+        anArray[2]=300;
+        anArray[3]=400;
+        anArray[4]=500;
+
+        System.out.println("Element at index 0: "+anArray[0]);
+        System.out.println("Element at index 1: "+anArray[1]);
+        System.out.println("Element at index 3: "+anArray[3]);
+
+
+    }
+}
