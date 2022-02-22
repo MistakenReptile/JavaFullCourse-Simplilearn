ab368634531750345fc81723f22235128eb541d2
MistakenReptile <hari.k.reddy0905@gmail.com>
2022-02-22 08:32:37 +0530

    first commit

---
+++ src/com/basics/operators/ComparisonOperators.java
@@ -0,0 +1,24 @@
+package com.basics.operators;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int firstNumber=10;
+        int secondNumber=20;
+
+        if(firstNumber==secondNumber)
+            System.out.println("First number == Second Number");
+
+        if(firstNumber!=secondNumber)
+            System.out.println("First number != Second Number");
+
+        if(firstNumber>secondNumber)
+            System.out.println("First number > Second Number");
+
+        if(firstNumber<secondNumber)
+            System.out.println("First number < Second Number");
+
+        if(firstNumber<=secondNumber)
+            System.out.println("First number <= Second Number");
+
+    }
+}
